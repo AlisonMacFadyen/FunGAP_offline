641e1508be24871d871ca8154cc182b51044f232
AlisonMacFadyen <alison.macfadyen86@gmail.com>
2025-09-24 15:30:09 +0100

    Modified versions of scripts to build image to work without internet

---
+++ docker/check_inputs.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python3
+
+'''
+Check if inputs are proper to run FunGAP
+Last updated: Jul 13, 2020
+'''
+
+import re
+import os
+import subprocess
+import sys
+
+from Bio import SeqIO
+
+from import_config import import_config
+
+
+def check_inputs(
+        trans_read_1, trans_read_2, trans_read_single, trans_bam,
+        genome_assembly, sister_proteome, busco_dataset):
+    '''Check input formats'''
+    print('Check input files...')
+    check_files_exists(trans_read_1, trans_read_2, trans_read_single)
+    trans_read_files = check_trans(
+        trans_read_1, trans_read_2, trans_read_single, trans_bam
+    )
+    check_assmebly(genome_assembly)
+    check_sister_proteome(sister_proteome)
+    check_busco_dataset(busco_dataset)
+    print('')
+    return trans_read_files
+
+
+def check_files_exists(trans_read_1, trans_read_2, trans_read_single):
+    '''Check if input files exist'''
+    if trans_read_1 and not os.path.exists(trans_read_1):
+        sys.exit(
+            '[ERROR] No such file {}'.format(os.path.basename(trans_read_1))
+        )
+    if trans_read_2 and not os.path.exists(trans_read_2):
+        sys.exit(
+            '[ERROR] No such file {}'.format(os.path.basename(trans_read_2))
+        )
+    if trans_read_single and not os.path.exists(trans_read_single):
+        sys.exit(
+            '[ERROR] No such file {}'.format(
+                os.path.basename(trans_read_single)
+            )
+        )
+
+
+def check_trans(trans_read_1, trans_read_2, trans_read_single, trans_bam):
+    '''Check transcriptome files'''
+    trans_read_files = []
+    if trans_read_1 and trans_read_2:
+        # Check extension
+        cond1 = trans_read_1.endswith('_1.fastq')
+        cond2 = trans_read_1.endswith('_1.fq')
+        cond3 = trans_read_2.endswith('_2.fastq')
+        cond4 = trans_read_2.endswith('_2.fq')
+        if not cond1 and not cond2:
+            error_message = (
+                '[ERROR] TRANS_READ_1 file name is incorrect. '
+                'Should be <prefix>_1.fastq. You provided {}'.format(
+                    trans_read_1
+                )
+            )
+            sys.exit(error_message)
+        elif not cond3 and not cond4:
+            error_message2 = (
+                '[ERROR] TRANS_READ_2 file name is incorrect. '
+                'Should be <prefix>_2.fastq. You provided {}'.format(
+                    trans_read_1
+                )
+            )
+            sys.exit(error_message2)
+
+        # Check prefix
+        prefix_1 = (
+            os.path.basename(trans_read_1)
+            .replace('_1.fastq', '')
+            .replace('_1.fq', '')
+        )
+        prefix_2 = (
+            os.path.basename(trans_read_2)
+            .replace('_2.fastq', '')
+            .replace('_2.fq', '')
+        )
+        if prefix_1 != prefix_2:
+            error_message3 = (
+                '[ERROR] Two paired-end trans_read_files should have same'
+                'prefix. <prefix>_1.f(ast)q and <prefix>_2.f(ast)q'
+            )
+            sys.exit(error_message3)
+
+        trans_read_files = [trans_read_1, trans_read_2]
+
+    elif trans_read_single:
+        # Check extension
+        cond5 = trans_read_single.endswith('_s.fastq')
+        cond6 = trans_read_single.endswith('_s.fq')
+        if not cond5 and not cond6:
+            error_message4 = (
+                '[ERROR] TRANS_READ_SINGLE file name is incorrect. Should be '
+                '<prefix>_s.fastq. You provided {}'.format(trans_read_single)
+            )
+            sys.exit(error_message4)
+        trans_read_files = [trans_read_single]
+
+    elif trans_bam:
+        trans_read_files = [trans_bam]
+
+    if not trans_read_files:
+        error_message5 = (
+            '[ERROR] You did not provide any transcriptome files: '
+            '-1 and -2, -U, or -A should be provided'
+        )
+        sys.exit(error_message5)
+
+    print('TRANS_READ_FILES is ok...')
+    return trans_read_files
+
+
+def check_assmebly(genome_assembly):
+    '''Check geonme assembly in FASTA'''
+    with open(genome_assembly, 'r') as handle:
+        fasta = SeqIO.parse(handle, 'fasta')
+        if not any(fasta):
+            sys.exit('[ERROR] FASTA file is invalid: {}'.format(
+                genome_assembly
+            ))
+
+        error_message6 = (
+            '[ERROR] FASTA defline contains "|" character, please remove and '
+            're-run'
+        )
+        for record in SeqIO.parse(handle, 'fasta'):
+            if '|' in record.id:
+                sys.exit(error_message6)
+
+    print('GENOME_ASSEMBLY is ok...')
+
+
+def check_sister_proteome(sister_proteome):
+    '''Check sister_proteome in FASTA'''
+    with open(sister_proteome, 'r') as handle:
+        fasta = SeqIO.parse(handle, 'fasta')
+        if not any(fasta):
+            sys.exit('[ERROR] FASTA file is invalid: {}'.format(
+                sister_proteome
+            ))
+    print('SISTER_PROTEOME is ok...')
+
+
+def check_busco_dataset(busco_dataset):
+    '''Check BUSCO dataset'''
+    busco_dbs = []
+    busco_dataset_found = False
+
+    with open("/workspace/FunGAP/busco_datasets.txt", 'r') as busco_list:
+        for line in busco_list:
+            busco_dbs.append(re.findall(r'\S+_odb10|\S+_odb12', line))
+    
+    print("Loaded 'busco_datasets.txt'.")
+    print("Now comparing datasets to user provided BUSCO dataset.")
+    
+    for dataset in busco_dbs:
+        if busco_dataset in dataset:
+            busco_dataset_found = True
+            
+    if busco_dataset_found:
+        print('BUSCO_DATASET is ok...')
+    else:
+        sys.exit(
+                 '[ERROR] Invalid BUSCO DATASET: {}. Run busco --list-datasets to '
+                 'get a full list available datasets'.format(busco_dataset)
+            )
---
+++ docker/run_busco.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python3
+
+'''
+Run BUSCO (v4.1.2 tested)
+
+- fungi_odb10
+  - ascomycota_odb10
+    - dothideomycetes_odb10
+      - capnodiales_odb10
+      - pleosporales_odb10
+    - eurotiomycetes_odb10
+      - chaetothyriales_odb10
+      - eurotiales_odb10
+      - onygenales_odb10
+    - leotiomycetes_odb10
+      - helotiales_odb10
+    - saccharomycetes_odb10
+    - sordariomycetes_odb10
+      - glomerellales_odb10
+      - hypocreales_odb10
+  - basidiomycota_odb10
+    - agaricomycetes_odb10
+      - agaricales_odb10
+      - boletales_odb10
+      - polyporales_odb10
+    - tremellomycetes_odb10
+  - microsporidia_odb10
+  - mucoromycota_odb10
+    - mucorales_odb10
+
+Input: protein FASTA file
+Output: BUSCO output in text
+Last updated: Aug 12, 2020
+'''
+
+import os
+from argparse import ArgumentParser
+
+from import_config import import_config
+from set_logging import set_logging
+
+# Parameters
+D_CONF = import_config()
+
+
+def main():
+    '''Main function'''
+    argparse_usage = ('run_busco.py -i <input_fasta> -d <lineage_dataset>')
+    parser = ArgumentParser(usage=argparse_usage)
+    parser.add_argument(
+        '-i', '--input_fasta', nargs=1, required=True,
+        help='Input protein FASTA file'
+    )
+    parser.add_argument(
+        '-d', '--lineage_dataset', nargs=1, required=True,
+        help='BUSCO lineage dataset (must be available locally in offline mode)'
+    )
+    parser.add_argument(
+        '-o', '--output_dir', nargs='?', default='busco_out',
+        help='Output directory (default: busco_out)'
+    )
+    parser.add_argument(
+        '-l', '--log_dir', nargs='?', default='logs',
+        help='Log directory (default: logs)'
+    )
+    parser.add_argument(
+        '--download_path', nargs='?', default=None,
+        help='Path to local BUSCO datasets (required with --offline)'
+    )
+    parser.add_argument(
+        '--offline', action='store_true',
+        help='Run BUSCO in offline mode (do not attempt downloads)'
+    )
+
+    args = parser.parse_args()
+    input_fasta = os.path.abspath(args.input_fasta[0])
+    lineage_dataset = args.lineage_dataset[0]
+    output_dir = os.path.abspath(args.output_dir)
+    log_dir = os.path.abspath(args.log_dir)
+    download_path = os.path.abspath(args.download_path) if args.download_path else None
+    offline = args.offline
+
+    # Create necessary dir
+    create_dir(output_dir, log_dir)
+
+    # Set logging
+    log_file = os.path.join(log_dir, 'run_busco.log')
+    logger_time, logger_txt = set_logging(log_file)
+
+    # Run functions
+    log_tup = (log_dir, logger_time, logger_txt)
+    run_busco(input_fasta, lineage_dataset, output_dir, log_tup,
+              offline=offline, download_path=download_path)
+
+
+def import_file(input_file):
+    '''Import file'''
+    with open(input_file) as f_in:
+        txt = list(line.rstrip() for line in f_in)
+    return txt
+
+
+def create_dir(output_dir, log_dir):
+    '''Create directories'''
+    # Output directory
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+
+    # Log directory
+    if not os.path.exists(log_dir):
+        os.mkdir(log_dir)
+
+
+def run_busco(input_fasta, lineage_dataset, output_dir, log_tup,
+              offline=False, download_path=None):
+    '''Run BUSCO'''
+    log_dir, logger_time, logger_txt = log_tup
+    busco_bin = D_CONF['BUSCO_PATH']
+    input_base = os.path.splitext(os.path.basename(input_fasta))[0]
+    logger_time.debug('START: BUSCO')
+
+    busco_full_table = os.path.join(
+        output_dir, input_base, 'run_{}'.format(lineage_dataset),
+        'full_table.tsv'
+    )
+
+    if not os.path.exists(busco_full_table):
+        log_file = os.path.join(
+            log_dir, 'busco_{}.log'.format(input_base)
+        )
+
+        # Build BUSCO command
+        command = (
+            '{} --mode proteins --out {} --in {} --out_path {} '
+            '--lineage_dataset {} --force'.format(
+                busco_bin, input_base, input_fasta, output_dir, lineage_dataset
+            )
+        )
+
+        # Add offline flags if requested
+        if offline:
+            command += ' --offline'
+        if download_path:
+            command += f' --download_path {download_path}'
+
+        # Redirect logs
+        command += f' > {log_file} 2>&1'
+
+        logger_txt.debug('[Run] %s', command)
+        os.system(command)
+    else:
+        logger_txt.debug('[Note] Running BUSCO has already been finished')
+
+    logger_time.debug('DONE : BUSCO')
+
+
+if __name__ == '__main__':
+    main()
